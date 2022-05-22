c758300f740e276f9fc1d31abaf46be5e7ccc78c
sz_skill <lumonexu@gmail.com>
2022-05-22 22:05:54 +0800

    feat: Add tests

---
+++ src/utils/randomColor.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+
+const randomColor = require("./randomColor");
+
+test("randomColor returns a color between 000000 and FFFFFF", () => {
+  expect(randomColor()).toBeGreaterThanOrEqual(0);
+  expect(randomColor()).toBeLessThanOrEqual(0xffffff);
+});
