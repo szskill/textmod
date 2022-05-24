2989678cab52a2df920e6bf203b21f913e9d956d
sz_skill <lumonexu@gmail.com>
2022-05-24 16:50:14 +0800

    feat: Add christian-minecraft-server

---
+++ src/commands/christian-minecraft-server.js
@@ -0,0 +1,34 @@
+// eslint-disable-next-line no-unused-vars
+const Discord = require("discord.js-light");
+const randomColor = require("../utils/randomColor");
+
+module.exports = {
+  name: "christian-minecraft-server",
+  description: "Stops swearing in your christian Minecraft server",
+  options: [{ name: "text", description: "The text to filter", type: String }],
+
+  /**
+   * @param {Discord.CommandInteraction} interaction
+   * @param {Discord.CommandInteractionOptionResolver} options
+   */
+  async execute(interaction, options) {
+    const text = options.getString("text");
+
+    let filtered = text
+      .replace("fuck", "f\\*\\**")
+      .replace("shit", "s\\*\\**")
+      .replace("bitch", "b\\*\\*\\**")
+      .replace("dick", "d\\*\\**")
+      .replace("ass", "a\\*\\*")
+      .replace("sex", "s\\**")
+      .replace("penis", "p\\*\\*\\**")
+      .replace("pussy", "p\\*\\*\\**")
+      .replace("boob", "b\\*\\**");
+
+    const embed = new Discord.MessageEmbed()
+      .setDescription(filtered)
+      .setColor(randomColor());
+
+    await interaction.reply({ embeds: [embed] });
+  },
+};
